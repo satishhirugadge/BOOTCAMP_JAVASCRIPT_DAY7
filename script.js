a9d3967bf0598246ca49bbdba00bc668992cc956
satish <you@example.com>
2020-05-27 23:54:36 -0500

    Day 7 JS Tasks

---
+++ script.js
@@ -0,0 +1,75 @@
+// TASK 01: 
+// Write a sample program to calculate the average of 5 subjects..
+
+// var subject1,subject2,subject3,subject4,subject5;
+// subject1=50;
+// subject2=60;
+// subject3=70;
+// subject4=80;
+// subject5=90;
+
+// var subject=(subject1+subject2+subject3+subject4+subject5)/5;
+// console.log(subject)
+
+
+// TASK 02:
+// Write a program to implement a decision making statement.
+//I am taking an example to find the tag used for the humans as per the age.
+
+// var age=25;
+// var name="Sam"
+// if (age<=12){
+//     console.log(name+" is a kid!!!")
+// }
+// else if(age>=13 && age<=19)
+// {
+//     console.log(name+" is a Teen!!!")
+// }
+// else if(age>=20 && age<=26)
+// {
+//     console.log(name+" is Adult!!!")
+// }
+// else{
+//     console.log(name+" is man!!!")
+// }
+
+
+// TASK 03:
+// Write a program to ask users to enter the number in the range of 1-20 
+//and see if the number is sorted or not. If not write a program to sort them. Also can enter maximum 5 values.
+
+// var num1 = prompt("1.....Enter any number from 1 to 20");
+// var num2 = prompt("2.....Enter any number from 1 to 20");
+// var num3 = prompt("3.....Enter any number from 1 to 20");
+// var num4 = prompt("4.....Enter any number from 1 to 20");
+// var num5 = prompt("5.....Enter any number from 1 to 20");
+
+// var num=[num1,num2,num3,num4,num5]
+// //I have sorted the number in the ascending order.
+// console.log(num.sort((a,b) => a-b));
+
+// TASK 04:
+//  Upload the code file on GitHub with a new repository.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
